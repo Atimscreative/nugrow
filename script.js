731fad2dd01a8f674d15f3583b9a459ff4d35d88
Abegunde Timilehin <timilehinsunday144@gmail.com>
2023-05-27 18:34:01 +0100

    Upload V2

--- script.js
+++ script.js
@@ -1,58 +1,18 @@
-(function () {
-  "use strict";
+"use strict";
 
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim();
-    if (all) {
-      return [...document.querySelectorAll(el)];
-    } else {
-      return document.querySelector(el);
-    }
-  };
+/*===== REMOVE NAVMENU WHEN USER CLICK EACH LINK =====*/
+// let header = document.querySelector("#header");
+const navLinks = document.querySelectorAll(".nav-link");
+const offCanva = document.querySelector("#offcanvasWithBothOptions");
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    offCanva.classList.remove("show");
+    const offcanvasBackdrop = document.querySelector(".offcanvas-backdrop");
+    offcanvasBackdrop.style.display = "none";
+  });
 });
 
-//Get The Button
-let BackTop = document.getElementById("backTop");
-
-// When User Scroll down 20px from top , show button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    BackTop.style.display = "block";
-  } else {
-    BackTop.style.display = "none";
-  }
-
-  // if (
-  //   document.body.scrollTop > 30 ||
-  //   (document.documentElement.scrollTop > 30 && navLinks)
-  // ) {
-  //   header.style.background = "#fff";
-  //   header.style.boxShadow = "0px 4px 30px -6px rgba(0,0,0,0.1)";
-  // } else {
-  //   header.style.background = "#3a376f";
-  //   header.style.boxShadow = "none";
-  // }
-}
-
-//when User scroll to the top of the document,
-BackTop.addEventListener("click", backToTop);
-
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-let header = document.querySelector("#header");
-let navLinks = document.querySelectorAll(".nav-link");
-
-/**/
+/*===== PORTFOLIO FILTERING =====*/
 let list = document.querySelectorAll(".filter-btn");
 let itemBox = document.querySelectorAll(".filter-item");
 
@@ -78,4 +38,51 @@ for (let i = 0; i < list.length; i++) {
       }
     }
   });
+}
+
+/*===== PORTFOLIO LIGHTBOX =====*/
+const gallery = document.querySelectorAll(".portfolio-wrap .img-fluid"),
+  btnIcon = document.querySelectorAll("#view-img"),
+  lightBox = document.querySelector(".lightbox"),
+  lightBoxImg = document.querySelector(".img-box"),
+  closeLightBox = document.querySelector(".close-box");
+
+btnIcon.forEach((icon, i) => {
+  icon.addEventListener("click", () => {
+    const img = gallery[i].src;
+
+    lightBox.classList.add("show-lightbox");
+    lightBoxImg.src = img;
+    console.log(lightBox);
+  });
+
+  // Closing lightbox
+  closeLightBox.addEventListener("click", (e) => {
+    lightBox.classList.remove("show-lightbox");
+  });
+});
+
+/*===== BACK TO TOP BUTTON =====*/
+let BackTop = document.getElementById("backTop");
+
+// When User Scroll down 20px from top , show button
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    BackTop.style.display = "block";
+  } else {
+    BackTop.style.display = "none";
+  }
+}
+
+//when User scroll to the top of the document,
+BackTop.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
